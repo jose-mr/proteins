34b9207554948983081d38e39571c9f29decccbf
ze <antoniojosemribeiro@gmail.com>
2024-04-05 16:38:22 +0100

    can add swissprot data to database

--- scripts/generate_datasets.py
+++ scripts/generate_datasets.py
@@ -6,7 +6,7 @@ def run():
 
     # update uniprot index file
     ## create index
-    Entry.create_dat_gz_index()
+    Entry.create_index()
     
     # Add and update entries
     Entry.create_and_update_all()
--- uniprot/models.py
+++ uniprot/models.py
@@ -1,8 +1,13 @@
+import io
+import gzip
 import subprocess
 import shutil
 
+from Bio import SeqIO
+
 from django.db import models
 from pseudoenzymes.settings import DATA_FOLDER
+from django.db import connection
 
 DAT_FILE = DATA_FOLDER / "uniprot/uniprot_sprot.dat.gz"
 CURRENT_RELEASE_FILE = DATA_FOLDER / "uniprot/reldate.txt"
@@ -14,14 +19,23 @@ class Entry(models.Model):
             primary_key=True,
             max_length=10,
             verbose_name="primary accession number",
-            )
+    )
     secondary_ac = models.JSONField(
             verbose_name="secondary accession numbers",
             default=list
-            )
+    )
+    name = models.TextField()
+    comment = models.TextField()
+    taxid = models.IntegerField(
+            verbose_name="NCBI TaxId"
+    )
+    keywords = models.JSONField(
+            default=list
+    )
+    seq = models.TextField()
 
     @classmethod
-    def create_dat_gz_index(cls, overwrite=False):
+    def create_index(cls, overwrite=False):
         """Creates a index for the dat gz file of UniProt"""
         print(f"Creating index for {DAT_FILE}")
         assert CURRENT_RELEASE_FILE.exists(), f"Cannot access {CURRENT_RELEASE_FILE}"
@@ -57,14 +71,14 @@ class Entry(models.Model):
         cls.create_and_update_list(uniprot_ids)
 
     @classmethod
-    def create_and_update_list(cls, uniprot_ids, in_swissprot=True):
+    def create_and_update_list(cls, uniprot_ids):
         """Adds the uniprot_ids in the list to the database or updates existing ones
 
         :param uniprot_ids: a container of uniprot accession numbers
         # :param in_swissprot: if these sequences belong to swissprot or trembl
         """
         records = cls.read_sequence_records(list(uniprot_ids), INDEX_FILE, DAT_FILE)
-        new_sequences = cls.create_and_update_sequences_from_records(records, in_swissprot=in_swissprot)
+        new_sequences = cls.create_and_update_from_records(records)
         print(f"{len(new_sequences)} sequences added to database")
         return new_sequences
 
@@ -79,13 +93,13 @@ class Entry(models.Model):
         :return: Bio Seq records
         """
         print("Reading Index")
-        index = cls.read_uniprot_index(index_file, uniprot_ids)
+        index = cls.read_index(index_file, uniprot_ids)
         print("Index Read")
         to_read_tuples = []
         for uniprot_id in uniprot_ids:
             if uniprot_id in index:
                 to_read_tuples.append(index[uniprot_id])
-        print("{} sequences found in index".format(len(to_read_tuples)))
+        print(f"{len(to_read_tuples)} sequences found in index")
         to_read_tuples = sorted(to_read_tuples, key=lambda tup: tup[0])
         with gzip.open(dat, 'rb') as dat_file:
             previous_end = 0
@@ -108,77 +122,37 @@ class Entry(models.Model):
         return set(uniprot_ids) - set(cls.objects.values_list("ac", flat=True))
 
     @classmethod
-    def create_and_update_sequences_from_records(cls, records, in_swissprot: bool):
+    def create_and_update_from_records(cls, records):
         """
         Creates and updates sequence objects from the record objects
         :param records: Bio Seq record with several sequences
         :param in_swissprot: if the sequences are in swissprot
         :return: the Sequence objects that were added to the database
         """
-        species_ncbi_to_dbid = dict(common.models.Species.objects.get_idx2id())
         to_create = []
-        exp_eco = set(common.models.EcoTerm.objects.experimental().values_list("idx", flat=True))
-        fields_to_check = ["name", "species_id", "in_reference_proteome", "function_experimental_evidence",
-                           "description", "caution", "misc"]
-        if in_swissprot:
-            existing = cls.objects.reviewed().get_idx2obj()
-        else:
-            existing = cls.objects.get_idx2obj()
+        fields_to_check = [
+                "name",
+                "taxid",
+                "comment",
+                "seq",
+                "keywords",
+                "secondary_ac",
+                ]
 
-        for no, record in enumerate(records):
+        existing = {entry.ac: entry for entry in cls.objects.all()}
+
+        print(len(connection.queries))
+        for record in records:
             name = record.description
             name = name[name.find("Full=")+5:]
             name = name.split(";")[0]
             name = name.split(" {")[0]
-            function_experimental_evidence = False
-            description = caution = misc = ""
-            if 'comment' in record.annotations:
-                for line in record.annotations['comment'].splitlines():
-                    if line.startswith('FUNCTION'):
-                        description = line[10:line.find("{")]
-                        start = 0
-                        while True:
-                            eco_position = line.find("ECO:", start)
-                            if eco_position == -1:
-                                break
-                            elif line[eco_position+4:eco_position+11] in exp_eco:
-                                function_experimental_evidence = True
-                                break
-                            else:
-                                start = eco_position + 1
-                    if line.startswith('CAUTION'):
-                        caution = line[9:line.find("{")]
-                        start = 0
-                        while True:
-                            eco_position = line.find("ECO:", start)
-                            if eco_position == -1:
-                                break
-                            elif line[eco_position+4:eco_position+11] in exp_eco:
-                                function_experimental_evidence = True
-                                break
-                            else:
-                                start = eco_position + 1
-                    if line.startswith('MISCELLANEOUS'):
-                        start = 0
-                        while True:
-                            eco_position = line.find("ECO:", start)
-                            if eco_position == -1:
-                                break
-                            elif line[eco_position+4:eco_position+11] in exp_eco:
-                                function_experimental_evidence = True
-                                break
-                            else:
-                                start = eco_position + 1
-                        misc = line[15:line.find("{")]
-            # in reference proteome
-            in_reference_proteome = 'Reference proteome' in record.annotations.get('keywords', '')
+
             taxid = int(record.annotations['ncbi_taxid'][0])
-            try:
-                species_id = species_ncbi_to_dbid[taxid]
-            except KeyError:
-                species = common.models.Species.objects.create(taxid=taxid)
-                species_id = species.id
-                species_ncbi_to_dbid[taxid] = species_id
+            comment = record.annotations.get("comment", "")
+            seq = record.seq
+            keywords = record.annotations.get("keywords", [])
+            secondary_ac = record.annotations["accessions"]
 
             if record.id in existing:
                 current_record = existing[record.id]
@@ -186,23 +160,42 @@ class Entry(models.Model):
                 for field in fields_to_check:
                     new_value = eval(field)
                     if getattr(current_record, field) != new_value:
-                        print("update field", field, new_value, getattr(current_record, field))
                         setattr(current_record, field, new_value)
                         needs_update = True
                 if needs_update:
                     print("updating ", current_record)
                     current_record.save()
             else:
-                to_create.append(Sequence(
-                    uniprot_id=record.id,
+                to_create.append(cls(
+                    ac=record.id,
                     name=name,
-                    sequence=record.seq,
-                    species_id=species_id,
-                    in_swissprot=in_swissprot,
-                    in_reference_proteome=in_reference_proteome,
-                    function_experimental_evidence=function_experimental_evidence,
-                    description=description,
-                    caution=caution,
-                    misc=misc,
+                    seq=seq,
+                    taxid=taxid,
+                    comment=comment,
+                    secondary_ac=secondary_ac
                 ))
-        return Sequence.objects.bulk_create(to_create, batch_size=1000)
+        print(len(connection.queries))
+        return cls.objects.bulk_create(to_create, batch_size=1000)
+
+    @classmethod
+    def read_index(cls, filename, uniprot_ids=None):
+        """Read index file with the byte offsets of the uniprot accession numbers
+
+        return only the byte offset of the requested uniprot ids or all by default
+        """
+        index_dict = {}
+        uniprot_ids = set(uniprot_ids) if uniprot_ids is not None else None
+        with open(filename, 'r') as index_file:
+            previous_entries = []
+            for line in index_file:
+                words = line.strip().split(":")
+                index = int(words[0])
+                these_uniprot_ids = set(words[1:])
+                for entry in previous_entries:
+                    index_dict[entry][1] = index - index_dict[entry][0]
+                previous_entries = []
+                for uniprot_id in these_uniprot_ids:
+                    if uniprot_ids is None or uniprot_id in uniprot_ids:
+                        index_dict[uniprot_id] = [index, -1]
+                        previous_entries.append(uniprot_id)
+        return index_dict
